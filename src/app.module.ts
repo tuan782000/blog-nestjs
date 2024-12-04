31570038647a4e92e37f7cfb1a4c23f26ff73f3b
“tuan7820” <thaituan7820@gmail.com>
2024-12-04 08:53:38 +0700

    connect nestjs với mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entities/user.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: 'localhost',
+            port: 3309,
+            username: 'root',
+            password: '',
+            database: 'Blog',
+            entities: [User], // đăng ký User để vào hàng tự sinh
+            synchronize: true // đoạn này xem entities có entities thì sinh ra bảng đó.
+        }),
+        UserModule
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,20 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+// các @ là các anotation - thư viện cung cấp - thư viện typeorm
+@Entity()
+export class User {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    first_name: string;
+
+    @Column()
+    last_name: string;
+
+    @Column()
+    password: string;
+
+    @Column({ default: 1 })
+    status: number; // trạng thái account
+}
